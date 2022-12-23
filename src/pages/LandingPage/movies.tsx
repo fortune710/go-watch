f3651c38f06f35f0d1ab37ed50f13797fc03e0e3
fortune710 <fortune0208@yahoo.com>
2022-12-23 21:37:25 +0100

    movie data

---
+++ src/pages/LandingPage/movies.tsx
@@ -0,0 +1,33 @@
+import Wednesday from '../../assets/wednesday.webp';
+import SheHulk from '../../assets/she-hulk.webp';
+import BlackPanther from '../../assets/black-panther.webp';
+import FamilyGuy from '../../assets/family-guy.webp';
+import HouseofTheDragon from '../../assets/house-of-the-dragon.webp';
+
+export const Movies = [
+    {
+        id: 1,
+        name: "Wednesday",
+        image: Wednesday,
+    },
+    {
+        id: 2,
+        name: "She Hulk",
+        image: SheHulk,
+    },
+    {
+        id: 3,
+        name: "Black Panther",
+        image: BlackPanther,
+    },
+    {
+        id: 4,
+        name: "Family Guy",
+        image: FamilyGuy,
+    },
+    {
+        id: 5,
+        name: "House of the Dragon",
+        image: HouseofTheDragon,
+    }
+]
