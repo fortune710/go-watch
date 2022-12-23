c8d3fe35bccc00c40d2f08e8697de9e0c3f9ba82
fortune710 <fortune0208@yahoo.com>
2022-12-23 21:49:46 +0100

    app component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+
+import { LandingPage } from './pages/LandingPage';
+
+function App() {
+  const API_KEY = "e77b613c447296755dab014d1426012a";
+  return (
+  <LandingPage/>  
+  )
+}
+
+export default App
