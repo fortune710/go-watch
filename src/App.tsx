6f336eaeff92f9db699ab0ad624f1d2ef892da32
fortune710 <fortune0208@yahoo.com>
2022-12-30 21:56:23 +0100

    added some routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,36 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 
 import { LandingPage } from './pages/LandingPage';
+import { HomePage } from './pages/Home';
+import { MovieDetailPage } from './pages/Movie';
+import { SearchPage } from './pages/SearchPage';
+import { TVShowPage } from './pages/TvShow';
+import { BottomNavbar } from './components';
+import { Container } from '@mui/material';
+
+const ComingSoon: React.FC = () => {
+  return(
+    <>
+      <Container sx={{ display:'flex', alignItems: 'center', justifyContent:"center", height: '100vh' }}>
+        <h3>Coming Soon</h3>
+      </Container>
+      <BottomNavbar/>
+    </>
+  )
+}
 
 function App() {
   const API_KEY = "e77b613c447296755dab014d1426012a";
   return (
-  <LandingPage/>  
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/search" element={<SearchPage/>} />
+      <Route path="/movie/:id" element={<MovieDetailPage/>} />
+      <Route path="/show/:id" element={<TVShowPage/>} />
+      <Route path="/favourites" element={<ComingSoon/>}/>
+      <Route path="/profile" element={<ComingSoon/>}/>
+    </Routes>
   )
 }
 
