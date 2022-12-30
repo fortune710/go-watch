40b3efae6e332a3807b3bdd1954b624b74f8313b
fortune710 <fortune0208@yahoo.com>
2022-12-30 22:59:35 +0100

    added tending shows

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { BottomNavbar, Searchbar, Sidemenu } from "../../components"
+import { BottomNavbar, Searchbar, Sidemenu, Slides } from "../../components"
 import axios from "axios";
 import styles from "./home.module.scss";
 import { useQuery } from "@tanstack/react-query";
@@ -7,6 +7,9 @@ import { useNavigate } from "react-router-dom";
 
 export const HomePage: React.FC = () => {
     const [trendingMovies, setTrendingMovies] = useState<any>([]);
+    const [trendingShows, setTrendingShows] = useState<any>([])
+    const navigate = useNavigate();
+
     /*
     const { isLoading, data:searchResults } = useQuery(["search-data"], () => {
         if(!query) return;
@@ -25,21 +28,56 @@ export const HomePage: React.FC = () => {
         .then((res) => {
             setTrendingMovies(res.data.results)
         })
-    })
+
+        axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=e77b613c447296755dab014d1426012a`)
+        .then((res) => {
+            setTrendingShows(res.data.results)
+        })
+    }, [])
 
     return(
         <>
             <Sidemenu/>
 
             <section className="sidemenu-page">                
-                <div>
-                    <h1>Trending Movies</h1>
+                <div className={styles.section}>
+                    <h2>Trending Movies</h2>
                     
-                    <div className={styles.scrollView}>
+                    <Slides>
                         {trendingMovies.map((movie: any) => (
-                            <img key={movie.id} title={movie.title} src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt={movie.title}/>
+                            <div className={styles.scrollCard} onClick={() => navigate(`/movie/${movie.id}`)}>
+                                <img key={movie.id} title={movie.title} src={`https://image.tmdb.org/t/p/w1280${movie?.poster_path}`} alt={movie.title}/>
+                            </div>
                         ))}
-                    </div>
+                    </Slides>
+                </div>
+
+                <div className={styles.section}>
+                    <h2>Trending Shows</h2>
+                    <Slides>
+                        {
+                            !trendingShows ? (
+                                <>
+                                <div className={styles.slideCards}/>
+                                <div className={styles.slideCards}/>
+                                <div className={styles.slideCards}/>
+                                <div className={styles.slideCards}/>
+                                <div className={styles.slideCards}/>
+                                <div className={styles.slideCards}/>
+                                <div className={styles.slideCards}/>                                
+                                </>
+                                )
+                            : <>
+                            {
+                                trendingShows.map((movie: any) => (
+                                    <div className={styles.scrollCard} onClick={() => navigate(`/show/${movie.id}`)}>
+                                        <img key={movie.id} title={movie.title} src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt={movie.title}/>
+                                    </div>
+                                ))
+                            }
+                            </>
+                        }
+                    </Slides>
                 </div>
             </section>
             <BottomNavbar/>
