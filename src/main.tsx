addbed8ebadfcc82b31dbcdac641ab2476a970a5
fortune710 <fortune0208@yahoo.com>
2022-12-30 21:56:03 +0100

    modified react query configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchInterval: 1500000,
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
