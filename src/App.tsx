604743b89cd2132bd7a9834a718cd6db78ef051e
fortune710 <fortune0208@yahoo.com>
2023-01-04 15:22:20 +0100

    added more routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,14 @@ import { MovieDetailPage } from './pages/Movie';
 import { SearchPage } from './pages/SearchPage';
 import { TVShowPage } from './pages/TvShow';
 import { BottomNavbar } from './components';
+import { Suspense, lazy, useEffect } from 'react';
 import { Container } from '@mui/material';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import { app } from './utils';
+import { LoginPage } from './pages/Login';
+import { ProfilePage } from './pages/Profile';
+
+const SignUpPage = lazy(() => import('./pages/SignUp').then(m=> ({ default: m.SignUpPage })))
 
 const ComingSoon: React.FC = () => {
   return(
@@ -21,17 +28,22 @@ const ComingSoon: React.FC = () => {
 }
 
 function App() {
+  const auth = getAuth(app);
   const API_KEY = "e77b613c447296755dab014d1426012a";
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/search" element={<SearchPage/>} />
-      <Route path="/movie/:id" element={<MovieDetailPage/>} />
-      <Route path="/show/:id" element={<TVShowPage/>} />
-      <Route path="/favourites" element={<ComingSoon/>}/>
-      <Route path="/profile" element={<ComingSoon/>}/>
-    </Routes>
+    <Suspense fallback={<h2>Loading...</h2>}>
+      <Routes>
+        <Route path="/"  element={<LandingPage/>} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage/>} />
+        <Route path="/movie/:id" element={<MovieDetailPage/>} />
+        <Route path="/show/:id" element={<TVShowPage/>} />
+        <Route path="/favourites" element={<ComingSoon/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </Suspense>
   )
 }
 
