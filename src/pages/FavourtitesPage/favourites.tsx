96f969ca4c9c7aed16b5cb896fba2a0608592bd5
fortune710 <fortune0208@yahoo.com>
2023-01-04 15:23:18 +0100

    created favourtites page

---
+++ src/pages/FavourtitesPage/favourites.tsx
@@ -0,0 +1,13 @@
+import { BottomNavbar, Sidemenu } from "../../components"
+
+export const FavouritesPage: React.FC = () => {
+    return(
+        <>
+            <Sidemenu/>
+            <main className="sidemenu-page">
+
+            </main>
+            <BottomNavbar/>
+        </>
+    )
+}
