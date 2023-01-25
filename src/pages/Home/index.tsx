d46e31535c6b4d1de5fed8eb24b50a1aac54911c
fortune710 <fortune0208@yahoo.com>
2023-01-25 12:57:09 +0100

    added skeleton for loading image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,11 +4,8 @@ import axios from "axios";
 import styles from "./home.module.scss";
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
-import { Skeleton } from "@mui/material";
 
 export const HomePage: React.FC = () => {
-    const [trendingMovies, setTrendingMovies] = useState<any>([]);
-    const [trendingShows, setTrendingShows] = useState<any>([])
     const navigate = useNavigate();
 
     /*
@@ -22,18 +19,24 @@ export const HomePage: React.FC = () => {
         return data
     })*/
 
-    
+    const { isLoading: loadingTrendingMovies, data: trendingMovies } = useQuery(["trending-movies"], async () => {
+        return await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=e77b613c447296755dab014d1426012a`)
+        .then((res) => {
+            return res.data.results
+        })
+    })
+
+    const { isLoading: loadingTrendingShows, data: trendingShows } = useQuery(["trending-shows"], async () => {
+
+        return await axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=e77b613c447296755dab014d1426012a`)
+        .then((res) => {
+            return res.data.results;
+        })
+    })
+
 
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=e77b613c447296755dab014d1426012a`)
-        .then((res) => {
-            setTrendingMovies(res.data.results)
-        })
 
-        axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=e77b613c447296755dab014d1426012a`)
-        .then((res) => {
-            setTrendingShows(res.data.results)
-        })
     }, [])
 
     return(
@@ -45,52 +48,64 @@ export const HomePage: React.FC = () => {
                     <h2>Trending Movies</h2>
                     
                     <Slides>
-                        {trendingMovies.map((movie: any) => {
-                            if(!movie.poster_path){
-                                return <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
-                            } else {
-                                return (
-                                <div className={styles.scrollCard} onClick={() => navigate(`/movie/${movie.id}`)}>
-                                    <img key={movie.id} title={movie.title} src={`https://image.tmdb.org/t/p/w1280${movie?.poster_path}`} alt={movie.title}/>
-                                </div>
-                                )
-                            }
+                        <Skeleton isVisible={loadingTrendingMovies}/>
+                        <Skeleton isVisible={loadingTrendingMovies}/>
+                        <Skeleton isVisible={loadingTrendingMovies}/>
+                        <Skeleton isVisible={loadingTrendingMovies}/>
+                        <Skeleton isVisible={loadingTrendingMovies}/>
+                        <Skeleton isVisible={loadingTrendingMovies}/>
+                        <Skeleton isVisible={loadingTrendingMovies}/>
+
+                    {
+                        trendingMovies?.map((movie: any) => {
+                            return (
+                            <div className={styles.scrollCard} onClick={() => navigate(`/movie/${movie.id}`)}>
+                                <img key={movie.id} title={movie.title} src={`https://image.tmdb.org/t/p/w1280${movie?.poster_path}`} alt={movie.title}/>
+                            </div>
+                            )
                         
                             }
-                        )}
+                        )
+                    }
                     </Slides>
                 </div>
 
                 <div className={styles.section}>
                     <h2>Trending Shows</h2>
                     <Slides>
-                        {
-                            !trendingShows ? (
-                                <>
-                                <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
-                                <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
-                                <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
-                                <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
-                                <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
-                                <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
-                                <Skeleton variant="rounded" width={'120px'} height={'180px'}/>
+                        <Skeleton isVisible={loadingTrendingShows}/>
+                        <Skeleton isVisible={loadingTrendingShows}/>
+                        <Skeleton isVisible={loadingTrendingShows}/>
+                        <Skeleton isVisible={loadingTrendingShows}/>
+                        <Skeleton isVisible={loadingTrendingShows}/>
+                        <Skeleton isVisible={loadingTrendingShows}/>
+                        <Skeleton isVisible={loadingTrendingShows}/>
 
-                                </>
-                                )
-                            : <>
-                            {
-                                trendingShows.map((movie: any) => (
+                        {
+                            trendingShows ? (
+                                trendingShows?.map((movie: any) => (
                                     <div className={styles.scrollCard} onClick={() => navigate(`/show/${movie.id}`)}>
                                         <img key={movie.id} title={movie.title} src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt={movie.title}/>
                                     </div>
-                                ))
-                            }
-                            </>
+                                )) 
+                            ): null
                         }
                     </Slides>
                 </div>
             </section>
             <BottomNavbar/>
         </>
+    )
+}
+
+interface SkeletonProps {
+    isVisible: boolean
+}
+
+const Skeleton: React.FC<SkeletonProps> = ({ isVisible }) => {
+    if(!isVisible) return null
+    
+    return(
+        <div className={styles.scrollCard}/>
     )
 }
