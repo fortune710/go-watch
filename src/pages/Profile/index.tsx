0a2d32e7b20015fa79d4536bbb80bcf024928007
fortune710 <fortune0208@yahoo.com>
2023-01-30 16:31:09 +0100

    restyled profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "@mui/material"
+import { Avatar, Button } from "@mui/material"
 import { BottomNavbar, Sidemenu } from "../../components"
 import { useAuth } from "../../hooks/useAuth";
 import styles from "./index.module.scss";
@@ -11,13 +11,20 @@ export const ProfilePage: React.FC = () => {
             <section className="sidemenu-page">
             {
                 user ? (
-                    <>
-                    <div>
-                        <Avatar className={styles.avatar} src={!user?.photoURL ? "" : user.photoURL}/>
+                    <div className={styles.profileContainer}>
+                        <Avatar 
+                            sx={{ width: 100, height: 100 }}
+                            src={!user?.photoURL ? "" : user.photoURL}
+                        />
                         <h3>{user?.displayName}</h3>
+                        <Button 
+                            variant="contained"
+                            sx={{ textTransform: "capitalize"}}
+                            color="error"
+                        >
+                            Sign Out
+                        </Button>
                     </div>
-                    
-                    </>
                 ): <p>You must be signed in to view profile info</p>
             }
             </section>
